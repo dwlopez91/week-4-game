625e43f0011019d4ae1d8113e6c3e8b276aa157a
David Lopez <dwlopez91@gmail.com>
2017-12-14 18:54:54 -0600

    Second Draft

--- Assets/javascript/game.js
+++ Assets/javascript/game.js
@@ -1,5 +1,20 @@
+var randomNumber = Math.floor(Math.random() *101 + 19);
+	$("#Number").text(randomNumber);
 
-		
+	var red = Math.floor(Math.random() *11 + 1);
+	var blue = Math.floor(Math.random() *11 + 1);
+	var green = Math.floor(Math.random() *11 + 1);
+	var yellow = Math.floor(Math.random() *11 + 1);
+
+	var wins = 0;
+	var losses = 0; 
+
+	var yourScore = 0;
+
+
+$( document ).ready(function() {
+
+function reset (){
 	var randomNumber = Math.floor(Math.random() *101 + 19);
 	$("#Number").text(randomNumber);
 
@@ -8,59 +23,66 @@
 	var green = Math.floor(Math.random() *11 + 1);
 	var yellow = Math.floor(Math.random() *11 + 1);
 
-	var wins = 0
-	var losses = 0 
+	var yourScore = 0;
+	
+}
 
-	var yourScore = 0
+$("#red").click(function(){
+	yourScore += red;
+	console.log(yourScore);
+	$("#totalScore").text(yourScore); 
+	checkScore();
+});	
+
+$("#blue").click(function(){
+	yourScore = yourScore + blue;
+	console.log(yourScore);
+	$("#totalScore").text(yourScore);
+	checkScore();
+});	
+
+$("#green").click(function(){
+	yourScore = yourScore + green;
+	console.log(yourScore);
+	$("#totalScore").text(yourScore);
+	checkScore();
+
+});	
+
+$("#yellow").click(function(){
+	yourScore = yourScore + yellow;
+	$("#totalScore").text(yourScore);
+	console.log(yourScore);
+	checkScore();
+});	
+
 	
 
-	
-		$("#red").click(function(){
-			yourScore = yourScore + red;
-			console.log(yourScore);
-			$("#totalScore").text(yourScore)
-		});	
+function winner() {
+	alert("You Win!"); 
+	wins++;
+	$("#wins").text(wins);
+	reset();	
+}
 
-		$("#blue").click(function(){
-			yourScore = yourScore + blue;
-			console.log(yourScore);
-			$("#totalScore").text(yourScore)
-		});	
+function loser() {
+	alert("You Lose!"); 
+	losses++;
+	$("#losses").text(losses);	
+	reset();	
+}
 
-		$("#green").click(function(){
-			yourScore = yourScore + green;
-			console.log(yourScore);
-			$("#totalScore").text(yourScore)
-		});	
 
-		$("#yellow").click(function(){
-			yourScore = yourScore + yellow;
-			$("#totalScore").text(yourScore)
-			console.log(yourScore);
-		});	
 
-	
-
-	function winner() {
-		alert("You Win!"); 
-		wins++;
-		$("#wins").text(wins)	
-
+function checkScore (){
+	if (yourScore === randomNumber) {
+	winner();
 	}
-
-	function loser() {
-		alert("You Lose!"); 
-		losses++;
-		$("#losses").text(losses)		
+	else if (yourScore > randomNumber) {
+	loser();
 	}
+}
+
+})
 
 
-	if (totalScore === randomNumber) {
-		winner();
-	}
-
-	else if (totalScore > randomNumber) {
-		loser();
-	}
-
-// needed: reset function and to figure out why if/else if statements aren't calling the functions
